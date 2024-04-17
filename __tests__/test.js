daeee0f370b78e472a31be31b3b6296c3bb460aa
PaulineGuesnon <pauline.guesnon@icloud.com>
2024-04-17 14:26:49 +0200

    # Changes to be committed: #	modified:   __tests__/test.js #

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,3 +2,15 @@ test('exemple de test', () => {
     expect(true).toBe(true);
   });
   
+
+test('Exemple de test', () => {
+// Arrange (Préparation) : Définissez les conditions initiales
+    const a = 1;
+    const b = 2;
+  
+    // Act (Action) : Exécutez le code à tester
+    const result = a + b;
+  
+    // Assert (Assertion) : Vérifiez le résultat
+    expect(result).toBe(3);
+  });
